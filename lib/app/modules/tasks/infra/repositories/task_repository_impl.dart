01c2fbc0b7e717a33ac1400de12a95d2a4e1af34
Gabriel Inacio <Gis191295*>
2020-09-30 11:32:58 -0300

    Melhorado tratamento de erros

--- lib/app/modules/tasks/infra/repositories/task_repository_impl.dart
+++ lib/app/modules/tasks/infra/repositories/task_repository_impl.dart
@@ -11,60 +11,37 @@ class TaskRepositoryImpl implements TaskRepository {
 
   @override
   Future<Either<String, int>> deleteTask(int ids) async {
-    try {
-      final result = await datasource.deleteTask(ids);
-      var response = result.fold(id, id);
-      if (response == null) {
-        return Left('Resposta nulla ao deletar!');
-      }
-      return Right(response);
-    } on DeleteError catch (e) {
-      return Left(e.message);
-    }
+    final result = await datasource.deleteTask(ids);
+    var response = result.fold(id, id);
+    if (response == null) return Left('Resposta nulla ao deletar!');
+    if (response is DeleteError) return Left(response.message);
+    return Right(response);
   }
 
   @override
   Future<Either<String, List<TaskModel>>> getTask() async {
-    try {
-      final result = await datasource.getTask();
-      var response = result.fold(id, id);
-      if (response == null) {
-        return Left('Resposta nula ao pegar os dados!');
-      }
-      if (response is GetError) {
-        return Left(response.message);
-      }
-      return Right(response);
-    } on GetError catch (e) {
-      return Left(e.message);
-    }
+    final result = await datasource.getTask();
+    var response = result.fold(id, id);
+    if (response == null) return Left('Resposta nula ao pegar os dados!');
+    if (response is GetError) return Left(response.message);
+    return Right(response);
   }
 
   @override
   Future<Either<String, TaskModel>> insertTask(TaskModel task) async {
-    try {
-      final result = await datasource.insertTask(task);
-      final response = result.fold(id, id);
-      if (response == null) {
-        return Left('Resposta nula ao inserir os dados!');
-      }
-      return Right(response);
-    } on InsertError catch (e) {
-      return Left(e.message);
-    }
+    final result = await datasource.insertTask(task);
+    final response = result.fold(id, id);
+    if (response == null) return Left('Resposta nula ao inserir os dados!');
+    if (response is InsertError) return Left(response.message);
+    return Right(response);
   }
 
   @override
   Future<Either<String, int>> updateTask(TaskModel task) async {
-    try {
-      final result = await datasource.updateTask(task);
-      var response = result.fold(id, id);
-      if (response == null) {
-        return Left('Resposta nula ao atualizar os dados!');
-      }
-      return Right(response);
-    } on UpdateError catch (e) {
-      return Left(e.message);
-    }
+    final result = await datasource.updateTask(task);
+    var response = result.fold(id, id);
+    if (response == null) return Left('Resposta nula ao atualizar os dados!');
+    if (response is UpdateError) return Left(response.message);
+    return Right(response);
   }
 }
